061adb499f686368113514a69ad893009dec69c7
Shiven Juneja <hi@shivenj.com>
2025-07-22 13:13:39 +0530

    Add debouncing API  to trigger vercel build

---
+++ app/api/deploy-vercel/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+let lastTriggeredAt = 0;
+
+export async function POST(req: NextRequest) {
+  const now = Date.now();
+  const debounceMinutes = 5;
+
+  if (now - lastTriggeredAt < debounceMinutes * 60 * 1000) {
+    return NextResponse.json(
+      { message: "Debounced, try later" },
+      { status: 200 }
+    );
+  }
+
+  lastTriggeredAt = now;
+
+  const vercelWebhookURL =
+    "https://api.vercel.com/v1/integrations/deploy/prj_tCKkuLBVagUrNFziCXD9daLTcwTe/08yJWdkX9M";
+
+  try {
+    const response = await fetch(vercelWebhookURL, { method: "POST" });
+    const data = await response.json();
+
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    console.error("Webhook error:", error);
+    return NextResponse.json(
+      { message: "Webhook request failed" },
+      { status: 500 }
+    );
+  }
+}
