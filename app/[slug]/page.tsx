e8f7aecbcb2eeb21dec3441335a16f2b0b61a18a
Keymouse IT <info@keymouseit.com>
2025-08-05 17:02:12 +0530

    add footer

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { DrupalService, getPageTemplateNew } from "@/lib/DrupalService";
+import {
+  DrupalService,
+  getNewFooter,
+  getPageTemplateNew,
+} from "@/lib/DrupalService";
 import { DrupalNode } from "next-drupal";
-import NotFoundPage from "@/components/NotFound";
 import DynamicTemplateServer from "./screen";
+import NotFoundPage from "@/components/NotFound";
 
 const TemplatePage = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
 
   // Fetch all necessary Drupal data
   const headerSection = await DrupalService.getHeaderSection();
-  const footerSection = await DrupalService.getFooterSection();
   const newData = await getPageTemplateNew();
+  const footerData = await getNewFooter();
 
   const matchedTemplate = newData.find((template: DrupalNode) =>
     template.field_page_slug.includes(slug)
@@ -21,13 +25,11 @@ const TemplatePage = async ({ params }: { params: { slug: string } }) => {
   }
 
   return (
-    <>
-      <DynamicTemplateServer
-        templateData={matchedTemplate}
-        headerData={headerSection[0]}
-        footerData={footerSection[0]}
-      />
-    </>
+    <DynamicTemplateServer
+      templateData={matchedTemplate}
+      headerData={headerSection[0]}
+      footerData={footerData[0]}
+    />
   );
 };
 
