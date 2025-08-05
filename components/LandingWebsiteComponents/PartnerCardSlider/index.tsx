e9b7a02f082d1b3d8cb3fc79c667b865aded19c6
Keymouse IT <info@keymouseit.com>
2025-08-05 18:43:10 +0530

    remove home route code

--- components/LandingWebsiteComponents/PartnerCardSlider/index.tsx
+++ components/LandingWebsiteComponents/PartnerCardSlider/index.tsx
@@ -1,159 +1,147 @@
-'use client';
-import React from 'react';
-import Slider from 'react-slick';
-import { DrupalNode } from 'next-drupal';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import { DrupalNode } from "next-drupal";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
 interface PartnerCardSliderProps {
-	sliderData: DrupalNode;
+  sliderData: DrupalNode;
 }
 
 const PartnerCardSlider: React.FC<PartnerCardSliderProps> = ({
-	sliderData,
+  sliderData,
 }) => {
-	const settings = {
-		dots: false,
-		arrows: false,
-		infinite: true,
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		autoplay: true,
-		speed: 8000,
-		autoplaySpeed: 100,
-		cssEase: 'linear',
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1,
-				}
-			},
-			{
-				breakpoint: 767,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1,
-				}
-			},
-		]
-	};
-	const settings2 = {
-		dots: false,
-		arrows: false,
-		infinite: true,
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		autoplay: true,
-		speed: 8000,
-		autoplaySpeed: 100,
-		cssEase: 'linear',
-		rtl: true,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1,
-				}
-			},
-			{
-				breakpoint: 767,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1,
-				}
-			},
-		]
-	};
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 8000,
+    autoplaySpeed: 100,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const settings2 = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 8000,
+    autoplaySpeed: 100,
+    cssEase: "linear",
+    rtl: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-	return (
-		<>
-			<div className='mb-72'>
-				<motion.h3
-					initial={{ opacity: 0, y: 40 }}
-					whileInView={{ opacity: 1, y: 0 }}
-					viewport={{ once: true }}
-					transition={{
-						duration: 0,
-					}}
-					className="pt-[82px] remove-animation-fluctuation desktop:text-[55px] text-numans mb-5 desktop:leading-[85px] text-center category-gradient text-clip text-[48px] leading-normal mobileMax:text-[35px] mobileMax:mb-3"
-				>
-					{sliderData?.field_title}
-				</motion.h3>
+  return (
+    <div className="mb-72">
+      <motion.h3
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 0,
+        }}
+        className="pt-[82px] remove-animation-fluctuation desktop:text-[55px] text-numans mb-5 desktop:leading-[85px] text-center category-gradient text-clip text-[48px] leading-normal mobileMax:text-[35px] mobileMax:mb-3"
+      >
+        {sliderData?.field_title}
+      </motion.h3>
 
-				<Slider {...settings}>
-					{sliderData?.field_add_section?.slice(0, 8).map((partnerCard: DrupalNode, index: number) => {
-						return (
-							<motion.div
-								key={index}
-								className="px-[15px] mb-[30px] mt-10 w-[16%] mobileMax:w-[50%] betweenMobileTab:w-[25%] mobileMax:mb-5 mobileMax:px-[10px]"
-							// initial={{ opacity: 0, y: 50 }}
-							// whileInView={{ opacity: 1, y: 0 }}
-							// viewport={{ once: true }}
-							// transition={{
-							// 	type: 'spring',
-							// 	duration2.8,
-							// }}
-							>
-								<Link
-									href="https://dev-mission.keymouseit.com"
-									target="_blank"
-									className="border-2 border-transparent hover:border-blueBorder transition block rounded-[25px] flex justify-center items-center bg-white px-[15px] py-[10px] box-border w-full card-shadow"
-								>
-									<div className="max-w-[160px] w-full h-[110px] max-h-[110px] min-w-[120px] min-h-[100px] overflow-hidden">
-										<Image
-											src={`https://dev-mission.keymouseit.com${partnerCard?.field_icon?.uri?.url}`}
-											alt="sponser img"
-											width={160}
-											height={100}
-											className="w-full h-full max-w-full object-scale-down min-w-[120px] min-h-[100px]"
-										/>
-									</div>
-								</Link>
-							</motion.div>
-						);
-					})}
-				</Slider>
+      <Slider {...settings}>
+        {sliderData?.field_add_section
+          ?.slice(0, 8)
+          .map((partnerCard: DrupalNode, index: number) => {
+            return (
+              <div
+                key={index}
+                className="px-[15px] mb-[30px] mt-10 w-[16%] mobileMax:w-[50%] betweenMobileTab:w-[25%] mobileMax:mb-5 mobileMax:px-[10px]"
+              >
+                <Link
+                  href={partnerCard?.field_link?.uri}
+                  target="_blank"
+                  className="border-2 border-transparent hover:border-blueBorder transition block rounded-[25px] flex justify-center items-center bg-white px-[15px] py-[10px] box-border w-full card-shadow"
+                >
+                  <div className="max-w-[160px] w-full h-[110px] max-h-[110px] min-w-[120px] min-h-[100px] overflow-hidden">
+                    <Image
+                      src={`https://dev-mission.keymouseit.com${partnerCard?.field_icon?.uri?.url}`}
+                      alt="img"
+                      width={160}
+                      height={100}
+                      className="w-full h-full max-w-full object-scale-down min-w-[120px] min-h-[100px]"
+                    />
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+      </Slider>
 
-				<Slider {...settings2}>
-					{sliderData?.field_add_section?.slice(9, sliderData?.length).map((partnerCard: DrupalNode, index: number) => {
-						return (
-							<motion.div
-								key={index}
-								className="px-[15px] mb-[30px] w-[16%] mobileMax:w-[50%] betweenMobileTab:w-[25%] mobileMax:mb-5 mobileMax:px-[10px]"
-							// initial={{ opacity: 0, y: 60 }}
-							// whileInView={{ opacity: 1, y: 0 }}
-							// viewport={{ once: true }}
-							// transition={{
-							// 	type: 'spring',
-							// 	duration: 3.2,
-							// }}
-							>
-								<Link
-									href="https://dev-mission.keymouseit.com"
-									target="_blank"
-									className="border-2 border-transparent hover:border-blueBorder transition block rounded-[25px] flex justify-center items-center bg-white px-[15px] py-[10px] box-border w-full card-shadow"
-								>
-									<div className="max-w-[160px] w-full h-[110px] max-h-[110px] min-w-[120px] min-h-[100px] overflow-hidden">
-										<Image
-											src={`https://dev-mission.keymouseit.com${partnerCard?.field_icon?.uri?.url}`}
-											alt="sponser img"
-											width={160}
-											height={100}
-											className="w-full h-full max-w-full object-scale-down min-w-[120px] min-h-[100px]"
-										/>
-									</div>
-								</Link>
-							</motion.div>
-						);
-					})}
-				</Slider>
-			</div>
-		</>
-	);
+      <Slider {...settings2}>
+        {sliderData?.field_add_section
+          ?.slice(9, sliderData?.length)
+          .map((partnerCard: DrupalNode, index: number) => {
+            return (
+              <div
+                key={index}
+                className="px-[15px] mb-[30px] w-[16%] mobileMax:w-[50%] betweenMobileTab:w-[25%] mobileMax:mb-5 mobileMax:px-[10px]"
+              >
+                <Link
+                  href={partnerCard?.field_link?.uri}
+                  target="_blank"
+                  className="border-2 border-transparent hover:border-blueBorder transition block rounded-[25px] flex justify-center items-center bg-white px-[15px] py-[10px] box-border w-full card-shadow"
+                >
+                  <div className="max-w-[160px] w-full h-[110px] max-h-[110px] min-w-[120px] min-h-[100px] overflow-hidden">
+                    <Image
+                      src={`https://dev-mission.keymouseit.com${partnerCard?.field_icon?.uri?.url}`}
+                      alt="img"
+                      width={160}
+                      height={100}
+                      className="w-full h-full max-w-full object-scale-down min-w-[120px] min-h-[100px]"
+                    />
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+      </Slider>
+    </div>
+  );
 };
 
 export default PartnerCardSlider;
