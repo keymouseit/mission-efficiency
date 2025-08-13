c5ef54dc18d91ed82a69727baf60099f98fd50c0
Keymouse IT <info@keymouseit.com>
2025-08-13 13:04:18 +0530

    fix home banner issue

--- components/LandingWebsiteComponents/BannerSection/index.tsx
+++ components/LandingWebsiteComponents/BannerSection/index.tsx
@@ -13,17 +13,19 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
   const moveValue = useTransform(() => scrollYProgress.get() * 4000);
 
   return (
-    <div className="banner-wrap-styling relative min-h-[80vh] mobileMax:min-h-[70vh] mobileMax:min-h-[85vh] overflow-hidden">
+    <div className="banner-wrap-styling relative min-h-[80vh] mobileMax:min-h-[70vh] overflow-hidden">
       <div className="top-[-20%] left-0 z-[-3] absolute pointer-events-none mobileMax:top-[-60%]">
         <DynamicImage
           width={632}
           height={665}
-          src="/static/images/left-home-hero.svg" alt="left-bg"
+          src="/static/images/left-home-hero.svg"
+          alt="left-bg"
         />
       </div>
       <div className="top-[-20%] right-0 z-[-2] absolute pointer-events-none mobileMax:top-[-60%]">
         <DynamicImage
-          src="/static/images/right-home-hero.svg" alt="right-bg"
+          src="/static/images/right-home-hero.svg"
+          alt="right-bg"
           width={526}
           height={645}
         />
@@ -34,15 +36,13 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
           <h1 className="mobileMax:px-1 title-green-gradient text-clip text-xlarge mb-3 mt-0 leading-tight text-center text-numans desktop:max-w-[680px] mx-auto mobileMax:max-w-full mobileMax:text-[42px]">
             {title}
           </h1>
-          <p className="text-[27px] text-white text-center --font-poppins mobileMax:text-small mobileMax:text-small">
+          <p className="text-[27px] text-white text-center --font-poppins mobileMax:text-small">
             {subtitle}
           </p>
         </div>
       </div>
       {/* banner-building image */}
-      <DynamicImage
-        fill
-        height={525}
+      <img
         src="/static/images/banner-building.svg"
         className="absolute z-[-1] bottom-0 w-full mobileMax:w-[700px] mobileMax:max-w-[700px] mobileMax:left-1/2 mobileMax:-translate-x-1/2 pointer-events-none"
         alt="building"
@@ -54,7 +54,7 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
           style={{
             left: moveValue,
           }}
-        >=
+        >
           <DynamicImage
             width={110}
             height={52}
@@ -66,7 +66,9 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
       <DynamicImage
         width={1880}
         height={18}
-        src="/static/images/white-curve.png" alt="curve" className="absolute z-[3] w-full bottom-[-2px] h-[18px] "
+        src="/static/images/white-curve.png"
+        alt="curve"
+        className="absolute z-[3] w-full bottom-[-2px] h-[18px] "
       />
     </div>
   );
