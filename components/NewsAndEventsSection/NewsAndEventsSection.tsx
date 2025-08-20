96fcaf0cd6d977c600426c49932fb62de3d0c1d0
Keymouse IT <info@keymouseit.com>
2025-08-20 15:27:29 +0530

    fix slider issue

--- components/NewsAndEventsSection/NewsAndEventsSection.tsx
+++ components/NewsAndEventsSection/NewsAndEventsSection.tsx
@@ -8,61 +8,63 @@ import CardSlider from "../CardSlider";
 
 type NewsAndEventsSectionProps = {
   data?: DrupalNode;
-  newsAndEventSlider?:DrupalNode[];
+  newsAndEventSlider?: DrupalNode[];
 };
 
-const  NewsAndEventsSection: React.FC<NewsAndEventsSectionProps> = ({
+const NewsAndEventsSection: React.FC<NewsAndEventsSectionProps> = ({
   data,
   newsAndEventSlider,
 }) => {
   return (
     <>
-      {newsAndEventSlider && newsAndEventSlider?.length > 0 && <div className="py-16 bg-mapGray mobileMax:py-10">
-        <div className="mini-container">
-          <motion.h3
-            initial={{ opacity: 0, y: 35 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0 }}
-            className="remove-animation-fluctuation desktop:text-[66px] desktop:leading-[85px] text-numans desktop:mb-[70px] text-center text-clip text-[48px] leading-[60px] mb-[50px] mobileMax:text-[28px] mobileMax:leading-normal mobileMax:mb-8"
-            style={{
-              backgroundImage:
-                "linear-gradient(282deg, #716ef8, #716ef8, #40abe7, #40abe7, #40c9e7)",
-            }}
-          >
-            News & Events
-          </motion.h3>
-
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0 }}
-            className="remove-animation-fluctuation mb-16 b-10 mobileMax:py-6"
-          >
-            <CardSlider
-              latestSliderData={newsAndEventSlider}
-              sliderData={data?.field_add_slide}
-              sliderConfig={data?.field_slider_configuration}
-            />
-          </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, y: 35 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0 }}
-            className="remove-animation-fluctuation flex justify-center"
-          >
-            <Link
-              href="/news"
-              className="flex justify-center items-center get-involve-btn modals-gradientBtn font-mediums text-white text-medium capitalize min-h-[55px] rounded-lg min-w-[200px] px-5"
+      {newsAndEventSlider && newsAndEventSlider?.length > 0 && (
+        <div className="py-16 bg-mapGray mobileMax:py-10">
+          <div className="mini-container">
+            <motion.h3
+              initial={{ opacity: 0, y: 35 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0 }}
+              className="remove-animation-fluctuation desktop:text-[66px] desktop:leading-[85px] text-numans desktop:mb-[70px] text-center text-clip text-[48px] leading-[60px] mb-[50px] mobileMax:text-[28px] mobileMax:leading-normal mobileMax:mb-8"
+              style={{
+                backgroundImage:
+                  "linear-gradient(282deg, #716ef8, #716ef8, #40abe7, #40abe7, #40c9e7)",
+              }}
             >
-              View All News & Events
-            </Link>
-          </motion.div>
+              News & Events
+            </motion.h3>
+
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0 }}
+              className="remove-animation-fluctuation mb-16 b-10 mobileMax:py-6"
+            >
+              <CardSlider
+                latestSliderData={newsAndEventSlider}
+                sliderData={data?.field_add_slide}
+                sliderConfig={data?.field_slider_configuration}
+              />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, y: 35 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0 }}
+              className="remove-animation-fluctuation flex justify-center"
+            >
+              <Link
+                href="/news"
+                className="flex justify-center items-center get-involve-btn modals-gradientBtn font-mediums text-white text-medium capitalize min-h-[55px] rounded-lg min-w-[200px] px-5"
+              >
+                View All News & Events
+              </Link>
+            </motion.div>
+          </div>
         </div>
-      </div>}
+      )}
     </>
   );
 };
--- components/NewsAndEventsSection/index.tsx
+++ components/NewsAndEventsSection/index.tsx
@@ -6,18 +6,22 @@ type NewsAndEventsSectionProps = {
   data: DrupalNode;
 };
 
-async function NewsAndEventBlockServer({
-  data,
-}: NewsAndEventsSectionProps) {
-  const allCards = await DrupalService.getNewsCards()
+async function NewsAndEventBlockServer({ data }: NewsAndEventsSectionProps) {
+  const allCards = await DrupalService.getNewsCards();
   const sortedAndLimitedCards = allCards
     ?.sort((a: DrupalNode, b: DrupalNode) => {
       const dateA = new Date(a.field_news_date);
       const dateB = new Date(b.field_news_date);
       return dateB.getTime() - dateA.getTime();
-    }).slice(0, 3);
+    })
+    .slice(0, 3);
 
-  return <NewsAndEventsSection data={data} newsAndEventSlider={sortedAndLimitedCards} />;
+  return (
+    <NewsAndEventsSection
+      data={data}
+      newsAndEventSlider={sortedAndLimitedCards}
+    />
+  );
 }
 
 export default NewsAndEventBlockServer;
